e6340a2db1a7142fb0e95e3e183016203a6e7dec
Juan Marcos Enriquez <juanmarcosenriquez@Juans-MacBook-Air-2.local>
2026-02-05 09:48:12 -0300

    Apply purchase validation filters

--- query_recompra_mismo_pos.sql
+++ query_recompra_mismo_pos.sql
@@ -45,15 +45,33 @@ SELECT
         SELECT COUNT(DISTINCT o2.id)
         FROM order_shortages os2
         INNER JOIN orders o2 ON o2.id = os2.order_id
+        INNER JOIN order_products op2
+            ON op2.id = os2.order_product_id
+            AND op2.quantity > 0
+            AND op2.deleted_at IS NULL
+        INNER JOIN points_of_sale pos2
+            ON pos2.id = o2.point_of_sale_id
+        INNER JOIN clients c2
+            ON c2.id = pos2.client_id
         WHERE os2.ean = os.ean
           AND o2.point_of_sale_id = o_falta.point_of_sale_id
+          AND o2.status_id = 2
+          AND c2.is_demo = 0
+          AND c2.deleted_at IS NULL
+          AND pos2.deleted_at IS NULL
           AND o2.created_at BETWEEN '2026-01-01 00:00:00' AND o_falta.created_at
     ) AS veces_falta_antes
 FROM order_shortages os
 INNER JOIN orders o_falta
     ON o_falta.id = os.order_id
+INNER JOIN order_products op_falta
+    ON op_falta.id = os.order_product_id
+    AND op_falta.quantity > 0
+    AND op_falta.deleted_at IS NULL
 INNER JOIN points_of_sale pos
     ON pos.id = o_falta.point_of_sale_id
+INNER JOIN clients c
+    ON c.id = pos.client_id
 LEFT JOIN api_products ap
     ON ap.order_product_id = os.order_product_id
 LEFT JOIN (
@@ -67,14 +85,24 @@ LEFT JOIN (
     FROM order_shortages os_inner
     INNER JOIN orders o_falta_inner
         ON o_falta_inner.id = os_inner.order_id
+    INNER JOIN order_products op_falta_inner
+        ON op_falta_inner.id = os_inner.order_product_id
+        AND op_falta_inner.quantity > 0
+        AND op_falta_inner.deleted_at IS NULL
+    INNER JOIN points_of_sale pos_inner
+        ON pos_inner.id = o_falta_inner.point_of_sale_id
+    INNER JOIN clients c_inner
+        ON c_inner.id = pos_inner.client_id
     INNER JOIN orders o_recompra_inner
         ON o_recompra_inner.point_of_sale_id = o_falta_inner.point_of_sale_id
         AND o_recompra_inner.id <> os_inner.order_id
         AND o_recompra_inner.created_at > o_falta_inner.created_at
         AND o_recompra_inner.created_at <= DATE_ADD(o_falta_inner.created_at, INTERVAL 10 DAY)
+        AND o_recompra_inner.status_id = 2
     INNER JOIN order_products op_inner
         ON op_inner.order_id = o_recompra_inner.id
-        AND (op_inner.deleted_at IS NULL)
+        AND op_inner.quantity > 0
+        AND op_inner.deleted_at IS NULL
         AND (
             op_inner.barcode = os_inner.ean
             OR op_inner.alternative_barcode1 = os_inner.ean
@@ -82,7 +110,11 @@ LEFT JOIN (
             OR op_inner.alternative_barcode3 = os_inner.ean
             OR op_inner.alternative_barcode4 = os_inner.ean
         )
-    WHERE o_falta_inner.created_at BETWEEN '2026-01-01 00:00:00' AND '2026-01-02 23:59:59'
+    WHERE o_falta_inner.status_id = 2
+      AND c_inner.is_demo = 0
+      AND c_inner.deleted_at IS NULL
+      AND pos_inner.deleted_at IS NULL
+      AND o_falta_inner.created_at BETWEEN '2026-01-01 00:00:00' AND '2026-01-02 23:59:59'
     GROUP BY os_inner.ean, os_inner.order_id, o_falta_inner.point_of_sale_id, o_falta_inner.created_at
 ) primera
     ON primera.ean = os.ean
@@ -93,7 +125,8 @@ LEFT JOIN orders o_recompra
     ON o_recompra.id = primera.order_id_primera_recompra
 LEFT JOIN order_products op_recompra
     ON op_recompra.order_id = o_recompra.id
-    AND (op_recompra.deleted_at IS NULL)
+    AND op_recompra.quantity > 0
+    AND op_recompra.deleted_at IS NULL
     AND (
         op_recompra.barcode = os.ean
         OR op_recompra.alternative_barcode1 = os.ean
@@ -105,4 +138,8 @@ LEFT JOIN order_shortages os_recompra
     ON os_recompra.order_id = o_recompra.id
     AND os_recompra.ean = os.ean
 WHERE o_falta.created_at BETWEEN '2026-01-01 00:00:00' AND '2026-01-02 23:59:59'
+  AND o_falta.status_id = 2
+  AND c.is_demo = 0
+  AND c.deleted_at IS NULL
+  AND pos.deleted_at IS NULL
 ORDER BY os.ean, o_falta.created_at;
